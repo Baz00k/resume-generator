06c52ed79962213e3c81c552d675c2ae6d798ba4
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-07-27 12:01:24 +0200

    style: autoformat

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -7,27 +7,25 @@ const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  "extensions": [".svelte", ...mdsvexConfig.extensions],
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: [vitePreprocess({}), mdsvex(mdsvexConfig)],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess({}), mdsvex(mdsvexConfig)],
 
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(
-      {
-        pages: 'build',
-        assets: 'build',
-        fallback: null,
-      }
-    ),
-    paths: {
-      base: dev ? '' : process.env.BASE_PATH,
-    },
-  }
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,14 @@
-const typography = require("@tailwindcss/typography");
+import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+	theme: {
+		extend: {}
+	},
 
-  plugins: [typography]
+	plugins: [typography]
 };
 
 module.exports = config;
