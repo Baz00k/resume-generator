fdc6f653fd14d5ebd777c731c39ce004c2f3c7bc
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2024-02-15 15:45:55 +0100

    fix: display header only on md+ devices

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,7 +3,7 @@
 	import { exportToPDF } from '$lib/exportToPDF';
 </script>
 
-<header class="flex items-center justify-end p-3 md:px-6 w-full sm:hidden print:hidden">
+<header class="hidden md:flex items-center justify-end p-3 md:px-6 w-full print:hidden">
 	<button
 		class="flex items-center justify-center bg-gray-200 hover:bg-gray-300 dark:bg-gray-100/10 dark:hover:bg-gray-100/20 py-2 px-4 rounded-full transition-colors duration-300 ease-in-out"
 		on:click={exportToPDF}
