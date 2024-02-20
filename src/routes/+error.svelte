5ef7d4c02655c3503fd8a90b8765bc066ca5884c
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2024-02-20 14:25:34 +0100

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+{#if $page.error}
+	<div class="flex h-full w-full flex-col items-center justify-center gap-4 p-4">
+		<h1 class="text-6xl">{$page.status}</h1>
+		<p class="text-lg">{$page.error.message}</p>
+	</div>
+{/if}
