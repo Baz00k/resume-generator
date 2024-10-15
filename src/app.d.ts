deae16ccb5d9e88d34e105f0b5f4d6dbd85ec5da
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2024-10-15 14:46:46 +0200

    feat: add ability to translate the resume

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,25 @@
+import type { ParaglideLocals } from '@inlang/paraglide-sveltekit';
+import type { AvailableLanguageTag } from '../../lib/paraglide/runtime';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			paraglide: ParaglideLocals<AvailableLanguageTag>;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
+}
+
+declare module '*.mdx' {
+	import type { SvelteComponent } from 'svelte';
+
+	export default class Comp extends SvelteComponent {}
+
+	export const metadata: Record<string, unknown>;
 }
 
 export {};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { i18n } from '$lib/i18n';
+
 	import '../app.postcss';
 	import Header from '$components/Header.svelte';
 </script>
 
-<Header />
-<main class="w-full px-2 py-4 print:p-0">
-	<div
-		class="mx-auto h-full min-h-[29.7cm] w-full min-w-min max-w-[21cm] overflow-clip rounded-lg bg-gray-50 dark:bg-gray-100/10 print:absolute print:left-0 print:top-0 print:m-0 print:w-full print:rounded-none"
-		id="resume"
-	>
-		<slot />
-	</div>
-</main>
+<ParaglideJS {i18n}>
+	<Header />
+	<main class="w-full px-2 py-4 print:p-0">
+		<div
+			class="mx-auto h-full min-h-[29.7cm] w-full min-w-min max-w-[21cm] overflow-clip rounded-lg bg-gray-50 dark:bg-gray-100/10 print:absolute print:left-0 print:top-0 print:m-0 print:w-full print:rounded-none"
+			id="resume"
+		>
+			<slot />
+		</div>
+	</main>
+</ParaglideJS>
